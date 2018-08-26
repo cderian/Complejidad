a7da766fc8c5b285c2cc39650c353f23a57d607d
cderian <c.derian_11@ciencias.unam.mx>
2018-08-25 22:30:40 -0500

    Práctica funcionando correctamente

--- P01/Grafica.java
+++ P01/Grafica.java
@@ -19,10 +19,13 @@ public class Grafica{
 	Graph grafica = new SingleGraph("Gráfica");
 
 	//Copia de los vértices de la gráfica
-	ArrayList<Integer> grafica2 = new ArrayList<Integer>();
+	ArrayList<Node> grafica2 = new ArrayList<Node>();
 
 	//Vértices que forman parte del Conjunto Independiente
-	ArrayList<Integer> vertices = new ArrayList<Integer>();
+	//ArrayList<Integer> vertices = new ArrayList<Integer>();
+	//ArrayList<Node> vertices = new ArrayList<Node>();
+	ArrayList<Vertice> vertices = new ArrayList<Vertice>();
+	ArrayList<Vertice> vertices_rojos = new ArrayList<Vertice>();
 
 	/**
 	 * Constructor de la gprafica
@@ -30,7 +33,7 @@ public class Grafica{
 	public Grafica(){
 
 		//El número 3 indica el grado promedio de la gráfica
-		RandomGenerator random_gen = new RandomGenerator(3);
+		RandomGenerator random_gen = new RandomGenerator();
 		random_gen.addSink(grafica);
 		random_gen.begin();
 
@@ -47,7 +50,8 @@ public class Grafica{
 
 		for(int i=0; i < grafica.getNodeCount(); i++){
 			//Hacemos una copia de los vértices de la gráfica
-			this.grafica2.add(i);
+			vertices.add(new Vertice(i));
+			
 			//Los vértices de la gráfica los coloreamos de color azul
 			grafica.getNode(i).addAttribute("ui.style", "fill-color: rgb(7,3,61);");
 		}
@@ -61,23 +65,43 @@ public class Grafica{
 	 */
 	private void obtenerCI(){
 
-		int size = this.grafica2.size();
+		/*int size = this.grafica2.size();
 
 		while(size > 0){
 
 			//Escogemos un vértice al azar para que sea parte del CI
 			int v = random.nextInt(size);
+			Node escogido = grafica2.get(v);
+
 			int r = random.nextInt(2);
 
 			if(r==1){
-				this.vertices.add(v);
+				this.vertices.add(escogido);
 				//El vértice escogido al azar lo coloreamos de rojo
 				this.grafica.getNode(v).setAttribute("ui.class", "rojo");
 			}
 
-			this.grafica2.remove(v);
+			this.grafica2.remove(escogido);
 			
 			size = this.grafica2.size();
+		}*/
+
+		int tam = vertices.size();
+
+		while(tam > 0){
+			//Escogemos un vértice al azar para que sea parte del CI
+			int n = random.nextInt(tam);
+			Vertice v = vertices.get(n);
+
+			int r = random.nextInt(2);
+
+			if(r==1){
+				vertices_rojos.add(v);
+				grafica.getNode(v.getId()).setAttribute("ui.class", "rojo");
+			}
+
+			vertices.remove(v);
+			tam = vertices.size();
 		}
 	}
 
@@ -93,17 +117,45 @@ public class Grafica{
 		//Y también, el resultado de la verificación
 		String resultado = "\nVértices (Rojos) del Conjunto Independiente:";
 
-		for(int v : vertices){
-			for(int u : vertices){
-				boolean b = grafica.getNode(v).hasEdgeBetween(u);
+		/*for(Node v : vertices){
+			for(Node u : vertices){
+				boolean b = v.hasEdgeBetween(u);
 				if(!b){
 					ind =b;
 				}
 			}
+		}*/
+
+
+		/*for (Vertice v : vertices_rojos) {
+			for (Vertice u : vertices_rojos) {
+				Node n = grafica.getNode(v.getId());
+				Node m = grafica.getNode(u.getId());
+				boolean b = n.hasEdgeBetween(m);
+
+				if(!b){
+					ind = b;
+					break;
+				}
+			}
+		}*/
+
+		for (int i=0; i<vertices_rojos.size()-1; i++) {
+			for(int j=1; j<vertices_rojos.size(); j++){
+				Node n = grafica.getNode(vertices_rojos.get(i).getId());
+				Node m = grafica.getNode(vertices_rojos.get(j).getId());
+
+				boolean b = n.hasEdgeBetween(m);
+
+				if(b){
+					ind = !b;
+					break;
+				}
+			}			
 		}
 
-		for(int v : vertices){
-			resultado+=" " + v;
+		for(Vertice v : vertices_rojos){
+			resultado+=" " + v.getId();
 		}
 
 		resultado+="\nEs Conjunto Independiente: "+ind+"\n";
---
+++ P01/Vertice.java
@@ -0,0 +1,14 @@
+public class Vertice{
+
+	int id;
+	boolean marca;
+
+	public Vertice(int id){
+		this.id = id;
+		this.marca = false;
+	}
+
+	public int getId(){
+		return this.id;
+	}
+}
